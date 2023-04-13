8883852677bed3954239b7cb4fe9c523471852b1
dylan <dylanc@lavishflo.com>
2023-04-13 09:46:12 -0400

    edit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import app from "./server";
-app.listen(3000 || process.env.PORT, () => {
+import * as dotenv from "dotenv";
+dotenv.config();
+app.listen(process.env.PORT || 5000, () => {
   console.log("Server started on port 3000");
 });
